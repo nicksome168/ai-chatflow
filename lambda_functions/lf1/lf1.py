f4e9550e9d9862e8f301b3825eb9e5f9bbcf3148
Jayesh Kumar <jayeshmotog@gmail.com>
2023-12-14 11:18:24 -0500

    Updated LF1 for Eventbridge

--- lambda_functions/lf1/lf1.py
+++ lambda_functions/lf1/lf1.py
@@ -1,8 +1,8 @@
-import boto3
 import json
 import logging
+import os
+import boto3
 from botocore.exceptions import ClientError
-
 
 # Initialize logging
 logger = logging.getLogger()
@@ -11,12 +11,10 @@ logger.setLevel(logging.INFO)
 # Configuration
 region = 'us-east-1'
 ses_client = boto3.client('ses', region_name=region)
-sqs_client = boto3.client('sqs')
-queue_url = 'https://sqs.us-east-1.amazonaws.com/764559909612/Q1'
 
 # Email Config
-sender_email_id = 'jayesh1302@gmail.com'
-recipient_email_id = 'jayesh1302@gmail.com'
+sender_email_id = os.getenv('SENDER_EMAIL_ID', 'jayesh1302@gmail.com')
+recipient_email_id = os.getenv('RECIPIENT_EMAIL_ID', 'jayesh1302@gmail.com')
 
 # Function to return a standardized response
 def _return_response(msg, statusCode):
@@ -28,75 +26,9 @@ def _return_response(msg, statusCode):
         "body": json.dumps(msg)
     }
 
-# Main Lambda handler function
-def lambda_handler(event, context):
-    logger.info("Polling from: {}".format(queue_url))
-    try:
-        # Receive messages from SQS
-        response = sqs_client.receive_message(
-            QueueUrl=queue_url,
-            MaxNumberOfMessages=10,
-            WaitTimeSeconds=7
-        )
-        response = {
-            'Messages': [
-                {
-                    'MessageId': 'dummy-message-id',
-                    'ReceiptHandle': 'dummy-receipt-handle',
-                    'Body': 'This is a test message body. This is a summary of the response.',
-                    'Attributes': {
-                        'ApproximateReceiveCount': '1',
-                        'SentTimestamp': '1234567890',
-                        'SenderId': 'AIDACKCEVSQ6C2EXAMPLE',
-                        'ApproximateFirstReceiveTimestamp': '1234567890'
-                    },
-                    'MD5OfBody': 'cee8bfae8a6d415f9e1b827b3ff258c6',
-                    'EventSource': 'aws:sqs',
-                    'EventSourceARN': 'arn:aws:sqs:us-east-1:123456789012:MyQueue',
-                    'AwsRegion': 'us-east-1'
-                }
-            ]
-        }
-        # Check if messages were received
-        if 'Messages' not in response:
-            msg = "No messages in queue."
-            logger.info(msg)
-            return _return_response(msg, 200)
-
-        # Process messages
-        for message in response['Messages']:
-            body = message['Body']
-            receipt_handle = message['ReceiptHandle']
-
-            # Here's where you'd parse the message body to extract email details, if needed
-            # For example purposes, we'll just send the SQS message body as the email body
-            send_email(ses_client, sender_email_id, recipient_email_id, 'AI Photo Album : Summary text from chat', body)
-
-            # Delete the message from the queue after processing
-            sqs_client.delete_message(
-                QueueUrl=queue_url,
-                ReceiptHandle=receipt_handle
-            )
-
-        # Return success response
-        msg = "Emails sent successfully."
-        logger.info(msg)
-        return _return_response(msg, 200)
-
-    except Exception as e:
-        # Log and return error response
-        logger.error(e)
-        return _return_response(str(e), 500)
-
 # Function to send an email using SES
 def send_email(ses_client, sender_email, recipient_email, subject, body_text):
     CHARSET = "UTF-8"
-    print('************************************')
-    print(body_text)
-    print(ses_client)
-    print(sender_email)
-    print(recipient_email)
-    print(subject)
     try:
         response = ses_client.send_email(
             Destination={
@@ -119,3 +51,21 @@ def send_email(ses_client, sender_email, recipient_email, subject, body_text):
         logger.info(f"Email sent to {recipient_email} with Message ID: {response['MessageId']}")
     except ClientError as e:
         logger.error(f"An error occurred: {e.response['Error']['Message']}")
+        raise
+
+# Main Lambda handler function
+def lambda_handler(event, context):
+    try:
+        # The 'event' parameter now contains the message from SQS provided by EventBridge
+        body = event['Records'][0]['body']
+        send_email(ses_client, sender_email_id, recipient_email_id, 'AI Photo Album : Summary text from chat', body)
+
+        # Log success message
+        msg = "Email sent successfully."
+        logger.info(msg)
+        return _return_response(msg, 200)
+
+    except Exception as e:
+        # Log and return error response
+        logger.error(e)
+        return _return_response(str(e), 500)
