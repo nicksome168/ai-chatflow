fd23989d40ec7ad71049d0003b736204f9c3f7f8
Nick Chao <yc6371@nyu.edu>
2023-12-16 12:56:27 -0500

    give node group admin access

--- terraform/test-eks/7-nodes.tf
+++ terraform/test-eks/7-nodes.tf
@@ -27,6 +27,10 @@ resource "aws_iam_role_policy_attachment" "nodes_amazon_ec2_container_registry_r
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.nodes.name
 }
+resource "aws_iam_role_policy_attachment" "admin" {
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  role       = aws_iam_role.nodes.name
+}
 
 resource "aws_eks_node_group" "private_nodes" {
   cluster_name    = aws_eks_cluster.demo.name
