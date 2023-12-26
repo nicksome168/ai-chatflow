9067c44fa553a72b5f6b2bfad3b116c18afec0c6
Jayesh Kumar <jayeshmotog@gmail.com>
2023-12-26 17:15:52 -0500

    Final Commit - Lambda functions

--- lambda_functions/lf1/lf1.py
+++ lambda_functions/lf1/lf1.py
@@ -10,11 +10,11 @@ logger.setLevel(logging.INFO)
 
 # Configuration
 region = 'us-east-1'
+dynamodb = boto3.client('dynamodb', region_name=region)
 ses_client = boto3.client('ses', region_name=region)
 
 # Email Config
-sender_email_id = os.getenv('SENDER_EMAIL_ID', 'jayesh1302@gmail.com')
-recipient_email_id = os.getenv('RECIPIENT_EMAIL_ID', 'jayesh1302@gmail.com')
+sender_email_id = os.getenv('SENDER_EMAIL_ID')
 
 # Function to return a standardized response
 def _return_response(msg, statusCode):
@@ -53,19 +53,49 @@ def send_email(ses_client, sender_email, recipient_email, subject, body_text):
         logger.error(f"An error occurred: {e.response['Error']['Message']}")
         raise
 
+# Function to get the email address from DynamoDB based on the username
+def get_email_for_username(username):
+    try:
+        response = dynamodb.get_item(
+            TableName='users',
+            Key={
+                'userName': {'S': username}
+            }
+        )
+        if 'Item' in response and 'email' in response['Item']:
+            email = response['Item']['email']['S']
+            logger.info(f"Email for user {username} found: {email}")
+            return email
+        else:
+            logger.error(f"No email found for user {username}. Sending to admin.")
+            adminEmail = jayesh1302@gmail.com
+            return adminEmail
+    except ClientError as e:
+        logger.error(f"Unable to get email for user {username}: {e.response['Error']['Message']}")
+        raise
+
 # Main Lambda handler function
 def lambda_handler(event, context):
     try:
-        # The 'event' parameter now contains the message from SQS provided by EventBridge
-        body = event['Records'][0]['body']
-        send_email(ses_client, sender_email_id, recipient_email_id, 'AI Photo Album : Summary text from chat', body)
+        # Parse the message from SQS
+        message = json.loads(event['Records'][0]['body'])
+        logger.info(f"Received SQS message: {message}")
+        roomID = message['room']
+        # Extract username and find the associated email
+        username = message['username']
+        recipient_email_id = get_email_for_username(username)
+        if recipient_email_id is None:
+            raise ValueError(f"Email for user {username} not found.")
+        # Send email to the recipient
+        send_email(ses_client, sender_email_id, recipient_email_id, 'AI ChatFlow : Summary text from '+roomID, message['result'])
 
-        # Log success message
-        msg = "Email sent successfully."
+        # Log and return success response
+        msg = f"Email sent successfully to {recipient_email_id}."
         logger.info(msg)
         return _return_response(msg, 200)
 
     except Exception as e:
         # Log and return error response
-        logger.error(e)
-        return _return_response(str(e), 500)
+        error_msg = f"Error processing the Lambda function: {str(e)}"
+        logger.error(error_msg)
+        return _return_response(error_msg, 500)
--- lambda_functions/lf2/lf2.py
+++ lambda_functions/lf2/lf2.py
@@ -1,69 +1,89 @@
 import json
-import requests
 import boto3
+from boto3.dynamodb.conditions import Attr
+import requests
 
 def lambda_handler(event, context):
+    # OpenAI and SQS
     api_key = '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'
     sqs_queue_url = 'https://sqs.us-east-1.amazonaws.com/764559909612/Q1'
+    # DynamoDB setup
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('messages')
+
+    # Extract room and userName from the event
+    room = event['room']
+    userName = event['userName']
     
+    print(room)
+    print(userName)
+
+    # Scan DynamoDB for messages in the given room
+    scan_kwargs = {
+        'FilterExpression': Attr('room').eq(room),
+        'Limit': 20 
+    }
+
+    # Perform the scan operation on DynamoDB table
+    scan_result = table.scan(**scan_kwargs)
+    print(scan_result)
+
+    # Concatenate messages into one big string
+    messages = scan_result.get('Items', [])
+    print(messages)
+    sentMessage = ' '.join(message['message'] for message in messages if 'message' in message)
+    print("***************************")
+    print(sentMessage)
+
     # Define the query
-    query = "Summarize the text in less than 100 words: " + "Exciting news! Our cloud computing project is making significant progress. Over the past few weeks, we've been diligently working on migrating our data and applications to the cloud, and I'm thrilled to share some updates. Firstly, our migration plan is proceeding smoothly. We've successfully moved a substantial portion of our data to the cloud, resulting in improved accessibility and scalability. This will enable us to handle increased workloads seamlessly.Additionally, our cloud infrastructure is proving to be highly reliable. We've conducted thorough testing, and the results have been very promising. Downtime has been minimized, and our systems are running more efficiently than ever."
-
-    # try:
-    #     headers = {
-    #         "Authorization": f"Bearer {api_key}",
-    #         "Content-Type": "application/json"
-    #     }
-
-    #     data = {
-    #         "model": "gpt-3.5-turbo",
-    #         "messages": [{"role": "user", "content": query}]
-    #     }
-
-    #     response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=data)
-        
-    #     if response.status_code == 200:
-    #         result = response.json().get('choices')[0].get('message').get('content').strip() if response.json().get('choices') else 'No response'
-            
-    #         # Send message to SQS queue
-    #         sqs_client = boto3.client('sqs')
-    #         sqs_response = sqs_client.send_message(
-    #             QueueUrl=sqs_queue_url,
-    #             MessageBody=json.dumps(result)
-    #         )
-            
-    #         return {
-    #             'statusCode': 200,
-    #             'body': json.dumps({'GPT-3 Response': result, 'SQS Response': sqs_response})
-    #         }
-    #     else:
-    #         return {
-    #             'statusCode': response.status_code,
-    #             'body': json.dumps("Error occurred: " + response.text)
-    #         }
-    # except Exception as e:
-    #     print("Error while querying OpenAI or sending to SQS: ", str(e))
-    #     return {
-    #         'statusCode': 500,
-    #         'body': json.dumps("Error occurred")
-    #     }
-
-
-    # TESTING CODE
+    query = "Summarize the text in less than 100 words: " + sentMessage;
+    
+    print("*******************")
+    print(query)
     try:
-        sqs_client = boto3.client('sqs')
-        sqs_response = sqs_client.send_message(
-            QueueUrl=sqs_queue_url,
-            MessageBody="This is the testing code for SQS queue"
-        )
-        
-        return {
-            'statusCode': 200,
-            'body': json.dumps({'GPT-3 Response': "This is testing code for response", 'SQS Response': sqs_response})
+        headers = {
+            "Authorization": f"Bearer {api_key}",
+            "Content-Type": "application/json"
         }
+
+        data = {
+            "model": "gpt-3.5-turbo",
+            "messages": [{"role": "user", "content": query}]
+        }
+
+        response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=data)
+        print("**********RESPONSE*********")
+        print(response)
+        if response.status_code == 200:
+            result = response.json().get('choices')[0].get('message').get('content').strip() if response.json().get('choices') else 'No response'
+            
+            # Prepare the message for SQS queue
+            sqs_message_body = {
+                'room': event['room'],
+                'username': event['userName'],
+                'result': result
+            }
+            
+            # Send message to SQS queue
+            sqs_client = boto3.client('sqs')
+            sqs_response = sqs_client.send_message(
+                QueueUrl=sqs_queue_url,
+                MessageBody=json.dumps(sqs_message_body)
+            )
+            
+            return {
+                'statusCode': 200,
+                'body': json.dumps({'GPT-3 Response': result, 'SQS Response': sqs_response})
+            }
+        else:
+            return {
+                'statusCode': response.status_code,
+                'body': json.dumps("Error occurred: " + response.text)
+            }
     except Exception as e:
-        print("Error whilesending to SQS: ", str(e))
         return {
             'statusCode': 500,
-            'body': json.dumps("Error occurred")
+            'body': json.dumps("Error occurred: " + str(e))
         }
+
+    
